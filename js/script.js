5e09360abf0266f4a03a88babac92536082a4a10
haymanot12 <81270389+haymanot12@users.noreply.github.com>
2022-02-05 23:09:42 -0600

    day two challenge

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
 multi line comment*/
 console.log('sewit');
 console.log('my favorite food is salmon');
+let words= "supercalifragilisticexpialidocious";
+console.log(words.length);
+let anyNum= 123;
+console.log(anyNum);
+let bigNum= BigInt(123456);
+console.log(bigNum);
+typeof(bigNum); 

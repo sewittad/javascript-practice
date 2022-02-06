7e7b92d3116e241b468c61856865ad5f1ac5b252
haymanot12 <81270389+haymanot12@users.noreply.github.com>
2022-02-06 01:31:56 -0600

    day3 challenge1 and challenge3

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ console.log(anyNum);
 let bigNum= BigInt(123456);
 console.log(bigNum);
 typeof(bigNum); 
+let firstName= prompt("write your first name");
+
+let splitedName= firstName.split('');
+let reversedName= splitedName.reverse();
+let joinedName= reversedName.join('');
+alert(joinedName);
+let numOne= prompt(); 
+let numTwo= prompt();
+let firstNum= parseInt(numOne);
+let secondNum= parseInt(numTwo);
+alert(firstName+secondNum);
